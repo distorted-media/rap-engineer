19611c688433e460a0dce638d254fa82dbd59480
luc <luc@forgotten.name>
2023-08-06 15:08:53 -0700

    changed chip

--- src/components/chip.tsx
+++ src/components/chip.tsx
@@ -6,16 +6,7 @@ import FormControl from '@mui/joy/FormControl';
 import FormLabel from '@mui/joy/FormLabel';
 import Button from '@mui/joy/Button';
 import Textarea from '@mui/joy/Textarea';
-
-
-
 import Text from '@mui/joy/Chip';
-
-
-
-
-
-
 
 export default function MyApp() {
   return <Chip>Welcome to Rap-Engineer, your personalized high quality rap maker</Chip>;
